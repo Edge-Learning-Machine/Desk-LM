7f5877ce5eab67b46002fd1f5ccc61f08b01d165
niccolomonti <niccodoria@msn.com>
2020-07-18 17:55:21 +0200

    main.py prepared for use with API

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import argparse
-
 import logger as lg
-
-
 import numpy as np
+from pymongo import MongoClient
+import requests
+import zipfile
+import os
 
 '''
 from sklearn.linear_model import LogisticRegression
@@ -74,148 +75,184 @@ for mean, stdev, param in zip(means, stds, params):
 '''
 
 
+def elm(id, app=None):
+    global zip_position, client, db, models, result
 
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--dataset')
+    parser.add_argument('-p', '--preprocess')
+    parser.add_argument('-e', '--estimator')
+    parser.add_argument('-s', '--selection')
+    parser.add_argument('-o', '--output')
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-d', '--dataset')
-parser.add_argument('-p', '--preprocess')
-parser.add_argument('-e', '--estimator')
-parser.add_argument('-s', '--selection')
-parser.add_argument('-o', '--output')
-args = parser.parse_args()
+    if id != 0:
+        parser.parse_args(['--dataset', "input/ds_api.json"])
+        parser.parse_args(['--estimator', "input/est_api.json"])
+        parser.parse_args(['--preprocess', "input/pp_api.json"])
+        parser.parse_args(['--selection', "input/ms_api.json"])
+        parser.parse_args(['--output', "input/output_api.json"])
 
-import sys
-print("Python version")
-print (sys.version)
-import numpy as np
-print("Numpy version")
-print(np.__version__) 
-import sklearn as skl
-print("Sklearn version")
-print(skl.__version__) 
+        client = MongoClient('localhost', 27017)
+        db = client['elm']
+        models = db['models']
+        result = models.find_one({'_id':id})
+        zip_position = result['output']
 
-sys.path.insert(1, 'config')
-import Dataset as cds
-try:
-    ds = cds.Dataset(args.dataset)
-except ValueError as err:
-    quit(err)
+        import utils.create_input_file as cif
+        cif.create_input_file(result)
 
-import Estimator as ce
-try:
-    esti = ce.Estimator.create(args.estimator, ds)
-except ValueError as err:
-    quit(err)
+    args = parser.parse_args()
 
-import Preprocess as pp
-try:
-    prep = pp.Preprocess(args.preprocess)
-except ValueError as err:
-    quit(err)
+    import sys
+    print("Python version")
+    print (sys.version)
+    import numpy as np
+    print("Numpy version")
+    print(np.__version__) 
+    import sklearn as skl
+    print("Sklearn version")
+    print(skl.__version__)
 
-import ModelSelection as ms
-try:
-    ms = ms.ModelSelection(args.selection, esti)
-except ValueError as err:
-    quit(err)
+    if id != 0:
+        app.logger.info("Start ELM training")
+        models.update_one({'_id':id}, {'$set':{'status':"Training --> 0%"}})
 
-# lg.initLogger(args.dataset, args.estimator)
-
-op = None
-training_size = None
-if args.output != None:
-    import Output
+    sys.path.insert(1, 'config')
+    import Dataset as cds
     try:
-        op = Output.Output(args.output)
+        ds = cds.Dataset(args.dataset)
     except ValueError as err:
         quit(err)
-    if esti.nick=='knn':
-        training_size = op.training_set_cap
+
+    import Estimator as ce
+    try:
+        esti = ce.Estimator.create(args.estimator, ds)
+    except ValueError as err:
+        quit(err)
+
+    import Preprocess as pp
+    try:
+        prep = pp.Preprocess(args.preprocess)
+    except ValueError as err:
+        quit(err)
+
+    import ModelSelection as ms
+    try:
+        ms = ms.ModelSelection(args.selection, esti)
+    except ValueError as err:
+        quit(err)
+
+    # lg.initLogger(args.dataset, args.estimator)
+
+    op = None
+    training_size = None
+    if args.output != None:
+        import Output
+        try:
+            op = Output.Output(args.output)
+        except ValueError as err:
+            quit(err)
+        if esti.nick=='knn':
+            training_size = op.training_set_cap
 
 
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(
-    ds.X, ds.y, train_size=training_size, test_size=ds.test_size, random_state=0)
+    from sklearn.model_selection import train_test_split
+    X_train, X_test, y_train, y_test = train_test_split(
+        ds.X, ds.y, train_size=training_size, test_size=ds.test_size, random_state=0)
 
-col_means = X_train.mean()
-X_train = X_train.fillna(col_means)
-X_test = X_test.fillna(col_means)
+    col_means = X_train.mean()
+    X_train = X_train.fillna(col_means)
+    X_test = X_test.fillna(col_means)
 
-best_estimator = esti.process(prep, ms, X_train, y_train)
-#print(best_estimator.score(X_test, y_test))
-y_pred = best_estimator.predict(X_test)
+    best_estimator = esti.process(prep, ms, X_train, y_train)
+    #print(best_estimator.score(X_test, y_test))
+    y_pred = best_estimator.predict(X_test)
 
-import sklearn.metrics as metrics
-if hasattr(ms, 'metrics_average'):
-    score = getattr(metrics, ms.metrics)(y_test, y_pred, average=ms.metrics_average)
-    print(f'{cv.metrics}, average={ms.metrics_average} in testing set: {score}')
-else:
-    if hasattr(ms, 'is_RMSE'):
-        score = getattr(metrics, ms.metrics)(y_test, y_pred, squared=not ms.is_RMSE)
-        pref = ''
-        if ms.is_RMSE:
-            pref='root_'
-        print(f'{pref}{ms.metrics} in testing set: {score}')
+    import sklearn.metrics as metrics
+    if hasattr(ms, 'metrics_average'):
+        score = getattr(metrics, ms.metrics)(y_test, y_pred, average=ms.metrics_average)
+        print(f'{cv.metrics}, average={ms.metrics_average} in testing set: {score}')
     else:
-        score = getattr(metrics, ms.metrics)(y_test, y_pred)
-        print(f'{ms.metrics} in testing set: {score}')
+        if hasattr(ms, 'is_RMSE'):
+            score = getattr(metrics, ms.metrics)(y_test, y_pred, squared=not ms.is_RMSE)
+            pref = ''
+            if ms.is_RMSE:
+                pref='root_'
+            print(f'{pref}{ms.metrics} in testing set: {score}')
+        else:
+            score = getattr(metrics, ms.metrics)(y_test, y_pred)
+            print(f'{ms.metrics} in testing set: {score}')
 
-'''
-import sklearn.metrics as metrics
-if cv.scoring == None:
-    if esti.is_regr:
-        score = metrics.mean_squared_error(y_test, y_pred)
+    '''
+    import sklearn.metrics as metrics
+    if cv.scoring == None:
+        if esti.is_regr:
+            score = metrics.mean_squared_error(y_test, y_pred)
+        else:
+            score = metrics.r2(y_test, y_pred)
     else:
-        score = metrics.r2(y_test, y_pred)
-else:
-    score = getattr(metrics, 'f1_score')(y_test, y_pred, average='weighted') #None
-print(score)
-'''
-sys.path.insert(1, 'output')
-import OutputMgr as omgr
-omgr.OutputMgr.cleanOutDir()
+        score = getattr(metrics, 'f1_score')(y_test, y_pred, average='weighted') #None
+    print(score)
+    '''
+    sys.path.insert(1, 'output')
+    import OutputMgr as omgr
+    omgr.OutputMgr.cleanOutDir()
 
-esti.output_manager.saveParams(best_estimator['esti'])
+    esti.output_manager.saveParams(best_estimator['esti'])
 
-sys.path.insert(1, 'output')
-import Preprocessing_OM
-if 'scale' in best_estimator.named_steps:
-    best_scaler = best_estimator['scale']
-else:
-    best_scaler = None
-if 'reduce_dims' in best_estimator.named_steps:
-    best_reduce_dims = best_estimator['reduce_dims']
-else:
-    best_reduce_dims = None
-Preprocessing_OM.savePPParams(best_scaler, best_reduce_dims, esti)
+    sys.path.insert(1, 'output')
+    import Preprocessing_OM
+    if 'scale' in best_estimator.named_steps:
+        best_scaler = best_estimator['scale']
+    else:
+        best_scaler = None
+    if 'reduce_dims' in best_estimator.named_steps:
+        best_reduce_dims = best_estimator['reduce_dims']
+    else:
+        best_reduce_dims = None
+    Preprocessing_OM.savePPParams(best_scaler, best_reduce_dims, esti)
 
-if esti.nick == 'knn':
-    omgr.OutputMgr.saveTrainingSet(X_train, y_train, esti)
+    if esti.nick == 'knn':
+        omgr.OutputMgr.saveTrainingSet(X_train, y_train, esti)
 
 
-if op != None:
-    if op.is_dataset_test:
-        if op.dataset_test_size == 1:
-            omgr.OutputMgr.saveTestingSet(X_test, y_test, esti)
-        elif op.dataset_test_size < 1:
-            n_tests = int(y_test * op.dataset_test_size.shape[0])
-            omgr.OutputMgr.saveTestingSet(X_test[0:n_tests], y_test[0:n_tests], esti)
-        elif op.dataset_test_size != None:
-            omgr.OutputMgr.saveTestingSet(X_test[0:op.dataset_test_size].shape[0], y_test[0:op.dataset_test_size].shape[0], esti)
-        elif op.dataset_test_size == None:
-            omgr.OutputMgr.saveTestingSet(X_test, y_test, esti)
+    if op != None:
+        if op.is_dataset_test:
+            if op.dataset_test_size == 1:
+                omgr.OutputMgr.saveTestingSet(X_test, y_test, esti)
+            elif op.dataset_test_size < 1:
+                n_tests = int(y_test * op.dataset_test_size.shape[0])
+                omgr.OutputMgr.saveTestingSet(X_test[0:n_tests], y_test[0:n_tests], esti)
+            elif op.dataset_test_size != None:
+                omgr.OutputMgr.saveTestingSet(X_test[0:op.dataset_test_size].shape[0], y_test[0:op.dataset_test_size].shape[0], esti)
+            elif op.dataset_test_size == None:
+                omgr.OutputMgr.saveTestingSet(X_test, y_test, esti)
 
-    if op.export_path != None:
-        from distutils.dir_util import copy_tree
-        omgr.OutputMgr.cleanSIMDirs(f'{op.export_path}/')
-        fromDirectory = f"./out/include"
-        toDirectory = f"{op.export_path}/dlm/include"
-        copy_tree(fromDirectory, toDirectory)
-        fromDirectory = f"./out/source"
-        toDirectory = f"{op.export_path}/dlm/source"
-        copy_tree(fromDirectory, toDirectory)
-        fromDirectory = f"./out/model"
-        toDirectory = f"{op.export_path}/dlm/model"
-        copy_tree(fromDirectory, toDirectory)
+            if id != 0:
+                with zipfile.ZipFile(zip_position, 'w') as zipobj:
+                    for root, dirs, files in os.walk('out/'):
+                        for file in files:
+                            zipobj.write(os.path.join(root, file))
 
-print('The end')
+        if op.export_path != None:
+            from distutils.dir_util import copy_tree
+            omgr.OutputMgr.cleanSIMDirs(f'{op.export_path}/')
+            fromDirectory = f"./out/include"
+            toDirectory = f"{op.export_path}/dlm/include"
+            copy_tree(fromDirectory, toDirectory)
+            fromDirectory = f"./out/source"
+            toDirectory = f"{op.export_path}/dlm/source"
+            copy_tree(fromDirectory, toDirectory)
+            fromDirectory = f"./out/model"
+            toDirectory = f"{op.export_path}/dlm/model"
+            copy_tree(fromDirectory, toDirectory)
+
+    print('The end')
+
+    if id != 0:
+        app.logger.info("End ELM training")
+        models.update_one({'_id':id}, {'$set':{'status':"Training --> 100%"}})
+
+if __name__ == '__main__':
+    #elm("46456aca-77f7-4670-949f-fc129e0d79f3")
+    elm(0)
