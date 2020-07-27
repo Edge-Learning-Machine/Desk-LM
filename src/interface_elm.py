09dd15cca588d0f17cf53240a12becc9a65581b5
niccolomonti <niccodoria@msn.com>
2020-07-27 14:19:49 +0200

    Created interface for elm

--- src/interface_elm.py
+++ src/interface_elm.py
@@ -1,16 +1,60 @@
 import sys
 import json
+import argparse
+import zipfile
+import os
 
+INPUT = 'input/'
 
+def int_elm(id, app, collection, model, path, status, api_errors):
 
-def int_elm(id, app, collection, status, result):
-    app.logger.info("Inizio addestramento ELM")
+    #configurazione dei file json
+    app.logger.info("Configurazione file json per elm")
 
+    if model['model']['webhook']:
+        del model['model']['webhook']
+    for element in model['model']:
+        with open(f'{INPUT}{element}_api.json', 'w') as file:
+            json.dump(model['model'][element], file, indent=4)
+
+    #aggiorno mongodb
+    try:
+        collection.update_one({'_id':id}, {'$set':{'status':status[3]}})
+    except:
+        app.logger.info(api_errors['no_db'])
+
+    sys.path.append('./')
+    from main import ELM
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--predict')
+    parser.add_argument('--store', action="store_true")
+    args = parser.parse_args()
+
+    args.dataset = f'{INPUT}ds_api.json'
+    args.preprocess = f'{INPUT}pp_api.json'
+    args.estimator = f'{INPUT}est_api.json'
+    args.selection = f'{INPUT}ms_api.json'
+    args.output = f'{INPUT}output_api.json'
+
+    if ELM.init(args) == 0:
+        ELM.process()
+        #aggiorno mongodb
+        try:
+            collection.update_one({'_id':id}, {'$set':{'status.perc':100}})
+        except:
+            app.logger.info(api_errors['no_db'])
     
-    
-    app.logger.info("Fine addestramento ELM")
-    return
+    #creazione zip
+    with zipfile.ZipFile(path['zip'] + model['output'], 'w') as f:
+        for root, dirs, files in os.walk(path['output']):
+            for file in files:
+                f.write(os.path.join(root, file))
 
+    #aggiorno mongodb
+    try:
+        collection.update_one({'_id':id}, {'$set':{'status':status[4]}})
+    except:
+        app.logger.info(api_errors['no_db'])
 
-
-
+    app.logger.info("Done")
--- src/server.py
+++ src/server.py
@@ -13,17 +13,24 @@ from datetime import datetime
 import threading
 import sys
 
+global database, path, status, api_errors
+
+BASE_TOKEN = 'src/BASE_TOKEN.json'
 
 database = {
     #"url": "mongodb://mongodb:27017/", 
     "url": "mongodb://localhost:27017/",
     "name": "elm",
-    "collection": "models"
+    "collections": [
+        "clients",
+        "models"
+    ]
 }
 
 path = {
-    "output": "/zip/",
-    "datasets": "/datasets/"
+    "datasets": "datasets/",
+    "output": "out/",
+    "zip": "zip/"
 }
 
 status = [
@@ -34,13 +41,28 @@ status = [
     { 'code': 4, 'description': 'Done'}
 ]
 
+api_errors = {
+    'no_token': 'Missing token',
+    'no_db': 'Database not found',
+    'unauth': 'Unauthorizated',
+    'no_req': 'No request contents',
+    'np_json': 'Request content not in JSON format',
+    'no_json_valid': 'Request format not in valid JSON',
+    'no_model': 'Model not found'
+}
+
+#creazone app flask
 app = Flask(__name__)
 port = int(os.environ.get('PORT', 5000))
 
-client = MongoClient(database['url'])
-cursor = client[database['name']]
-collection = cursor[database['collection']]
+#connessione al database
+clientDB = MongoClient(database['url'])
+cursor = clientDB[database['name']]
+clients = cursor[database['collections'][0]]
+models = cursor[database['collections'][1]]
 
+
+#mongo = MongoDB(app, database)
 
 def answer(content, status):
     if(status != 200):
@@ -50,15 +72,27 @@ def answer(content, status):
 
 @app.route('/model', methods=['POST'])
 def set_model():
+    #verifico che utente sia autorizzato
+    if not request.headers.get('Authorization'):
+        return answer(api_errors['no_token'], 400)
+
+    try:
+        client = clients.find_one({'token':request.headers['Authorization']})
+    except:
+        return answer(api_errors['no_db'], 404)
+
+    if not client:
+        return answer(api_errors['unauth'], 401)
+
+    #verifico che la richiesta sia presente e in formato JSON valido
     if (not request.data):
-        return answer('No request contents', 400)
+        return answer(api_errors['no_req'], 400)
     if (not request.is_json):
-        return answer('Request content not in JSON format', 400)
+        return answer(api_errors['no_json'], 400)
     try:
         jsonpickle.decode(request.data)
     except ValueError as e:
-        return answer('Request format not in valid JSON: ' + e, 400)
-
+        return answer(api_errors['no_json_valid'] + ': ' + e, 400)
     content = request.get_json()
 
     # verifico che richiesta abbia tutti i campi necessari
@@ -68,56 +102,86 @@ def set_model():
         if not parameter in content:
             return answer("Missing parameter: '" + parameter + "'", 400)
     '''
+    #creazione nuovo modello
+    new_model = {}
+    new_model['model'] = content
 
     #aggiungo valori in risposta
-    content['_id'] = str(uuid.uuid4())
-    content['status'] = status[0]
-    content['output'] = content['_id'] +'.zip'
-    content['timestamp'] = str(datetime.now())
+    new_model['_id'] = str(uuid.uuid4())
+    new_model['status'] = status[0]
+    new_model['output'] = new_model['_id'] +'.zip'
+    new_model['timestamp'] = str(datetime.now())
 
     #inserisco in mongodb
     try:
-        collection.insert_one(content)
+        models.insert_one(new_model)
     except:
-        return answer('Database not connected', 400)
+        return answer(api_errors['no_db'], 404)
 
-    return answer(content, 200)
+    return answer(new_model, 200)
 
 
 @app.route('/model/<id>', methods=['GET'])
 def get_model(id):
+    #verifico che utente sia autorizzato
+    if not request.headers.get('Authorization'):
+        return answer(api_errors['no_token'], 400)
+
     try:
-        result = collection.find_one({'_id':id})
+        client = clients.find_one({'token':request.headers['Authorization']})
     except:
-        return answer('Database not connected', 400)
+        return answer(api_errors['no_db'], 404)
+
+    if not client:
+        return answer(api_errors['unauth'], 401)
+
+    #recupero modello da mongo
+    try:
+        result = models.find_one({'_id':id})
+    except:
+        return answer(api_errors['no_db'], 404)
 
     if not result:
-        return answer('Model not existing', 400)
+        return answer(api_errors['no_model'], 400)
 
     return answer(result, 200)
 
 
 @app.route('/model/<id>/trainingset', methods=['POST'])
 def upload_csv(id):
-    #verifico che vi sia il file csv
+    #verifico che utente sia autorizzato    
+    if not request.headers.get('Authorization'):
+        return answer(api_errors['no_token'], 400)
+
+    try:
+        client = clients.find_one({'token':request.headers['Authorization']})
+    except:
+        return answer(api_errors['no_db'], 404)
+
+    if not client:
+        return answer(api_errors['unauth'], 401)
+
+    #verifico che vi sia il file csv allegato
     if not request.files.get('file'):
         return answer("Missing csv file", 400)
     
     #verifico che vi siano i parametri necessari
+    '''
     model_parameters = ['target_column','test_size']
     for parameter in model_parameters:
         if not parameter in request.form:
             return answer("Missing parameter: '" + parameter + "'", 400)
+    '''
 
     #recupero da mongodb 
     try:
-        result = collection.find_one({'_id':id})
+        result = models.find_one({'_id':id})
     except:
-        return answer('Database not connected', 400)
+        return answer(api_errors['no_db'], 404)
 
     #se id non esiste
     if not result:
-        return answer("Model not existing", 400)
+        return answer(api_errors['no_model'], 400)
 
     #Salva il file csv con il nuovo nome
     try:
@@ -130,55 +194,72 @@ def upload_csv(id):
 
 
     #aggiungo configurazione dataset
-    result['d'] = {}
-    result['d']['path'] = path['datasets'] + file_name #
-    result['d']['target_column'] = request.form['target_column']
-    result['d']['test_size'] = float(request.form['test_size'])
+    result['model']['ds'] = {}
+    result['model']['ds']['path'] = path['datasets'] + file_name #
+    result['model']['ds']['target_column'] = request.form['target_column']
+    result['model']['ds']['test_size'] = float(request.form['test_size'])
     
-    vector_parameter = ['skip_rows', 'skip_columns']
-    for parameter in vector_parameter:
+    vector_parameters = ['skip_rows', 'skip_columns']
+    for parameter in vector_parameters:
         if request.form.get(parameter):
-            result['d'][parameter] = request.form.getlist(parameter)
+            result['model']['ds'][parameter] = request.form.getlist(parameter)
 
-    scalar_parameter = ['sep', 'decimal','categorical_multiclass']
-    for parameter in scalar_parameter:
+    scalar_parameters = ['sep', 'decimal','categorical_multiclass']
+    for parameter in scalar_parameters:
         if request.form.get(parameter):
-            result['d'][parameter] = request.form.get(parameter)
+            result['model']['ds'][parameter] = request.form.get(parameter)
     
     #aggiorno lo stato
     result['status'] = status[1]
 
     #aggiorno mongodb
     try:
-        collection.update_one({'_id':id}, {'$set':result})
+        models.update_one({'_id':id}, {'$set':result})
     except:
-        return answer("Database not connected", 400)
+        return answer(api_errors['no_db'], 404)
 
     return answer(result, 200)
 
 
 @app.route('/model/<id>', methods=['PUT'])
 def training(id):
+    #verifico che utente sia autorizzato    
+    if not request.headers.get('Authorization'):
+        return answer(api_errors['no_token'], 400)
+
+    try:
+        client = clients.find_one({'token':request.headers['Authorization']})
+    except:
+        return answer(api_errors['no_db'], 404)
+
+    if not client:
+        return answer(api_errors['unauth'], 401)
+    
+    #verifico che la richiesta sia presente e in formato JSON valido
     if (not request.data):
-        return answer('No request contents', 400)
+        return answer(api_errors['no_req'], 400)
     if (not request.is_json):
-        return answer('Request content not in JSON format', 400)
+        return answer(api_errors['no_json'], 400)
     try:
         jsonpickle.decode(request.data)
     except ValueError as e:
-        return answer('Request format not in valid JSON: ' + e, 400)
+        return answer(api_errors['no_json_valid'] + ': ' + e, 400)
 
     content = request.get_json()
 
     #recupero modello da mongodb
     try:
-        result = collection.find_one({'_id':id})
+        result = models.find_one({'_id':id})
     except:
-        return answer('Database not connected', 400)
+        return answer(api_errors['no_db'], 404)
 
     #verifico che modello esista
     if not result:
-        return answer("Model not existing", 400)
+        return answer(api_errors['no_model'], 400)
+
+    #e che sia stato inserito il trainingset
+    if result['status']['code'] < 1:
+        return answer('You must first load the training set', 400)
 
     #verifico body
     if not content['evaluate']:
@@ -186,14 +267,14 @@ def training(id):
 
     #aggiorno stato e mongodb
     try:
-        collection.update_one({'_id':id}, {'$set':{'status':status[2]}})
-        result = collection.find_one({'_id':id})
+        models.update_one({'_id':id}, {'$set':{'status':status[2]}})
+        result = models.find_one({'_id':id})
     except:
-        return answer('Database not connected', 400)
+        return answer(api_errors['no_db'], 404)
 
     # avvio thread per addestramento elm in parallelo
     import interface_elm
-    thread = threading.Thread(target=interface_elm.int_elm, args=(id, app, collection, status, result, ))
+    thread = threading.Thread(target=interface_elm.int_elm, args=(id, app, models, result, path, status, api_errors, ))
     thread.start()
 
     return answer(result, 200)
@@ -201,17 +282,29 @@ def training(id):
 
 @app.route('/model/<id>/<output>', methods=['GET'])
 def download(id, output):
+    #verifico che utente sia autorizzato    
+    if not request.headers.get('Authorization'):
+        return answer(api_errors['no_token'], 400)
+
+    try:
+        client = clients.find_one({'token':request.headers['Authorization']})
+    except:
+        return answer(api_errors['no_db'], 404)
+
+    if not client:
+        return answer(api_errors['unauth'], 401)
+
     #recupero modello da mongodb
     try:
-        result = collection.find_one({'_id':id})
+        result = models.find_one({'_id':id})
     except:
-        return answer('Database not connected', 400)
+        return answer(api_errors['no_db'], 404)
 
     #verifico che modello sia addestrato
     if result['status']['code'] != 4:
         return answer("Model not trained yet", 400)
 
-    path_zip = '..' + path['output'] + output
+    path_zip = path['zip'] + output
 
     return send_file(path_zip, as_attachment=True)
 
@@ -222,4 +315,12 @@ def notfound(error):
 
 
 if __name__ == '__main__':
+    try:
+        results = clients.find({})
+        if len(list(results)) == 0:      
+            client = json.load(open(BASE_TOKEN))
+            clients.insert_one(client)
+    except:
+        quit(api_errors['no_db'])
+
     app.run(debug=True, host='0.0.0.0', port=port, ssl_context='adhoc')
