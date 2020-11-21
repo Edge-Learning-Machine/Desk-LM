c6403adebefc84b20530e8119cb4c725984462fd
alessio-capello <alessio.capello.93@gmail.com>
2020-11-21 03:24:52 +0100

    cambiati i define per rimuovere ambiguità tra PCA e Test_Set

--- output/Preprocessing_OM.py
+++ output/Preprocessing_OM.py
@@ -24,23 +24,23 @@ def savePPParams(scaler, reduce_dims, estimator):
     myFile.write(f"#ifndef PPPARAMS_H\n")
     myFile.write(f"#define PPPARAMS_H\n\n")
 
-    myFile.write(f"#ifndef N_FEATURE\n")
-    myFile.write(f"#define N_FEATURE {pca_components.shape[0]}\n")
+    myFile.write(f"#ifndef PCA_N_FEATURE\n")
+    myFile.write(f"#define PCA_N_FEATURE {pca_components.shape[0]}\n")
     myFile.write(f"#endif\n\n")
-    myFile.write(f"#ifndef N_ORIG_FEATURE\n")
-    myFile.write(f"#define N_ORIG_FEATURE {pca_components.shape[1]}\n")
+    myFile.write(f"#ifndef PCA_N_ORIG_FEATURE\n")
+    myFile.write(f"#define PCA_N_ORIG_FEATURE {pca_components.shape[1]}\n")
     myFile.write(f"#endif\n\n")
-    myFile.write(f"extern float pca_components[N_FEATURE][N_ORIG_FEATURE];\n")
+    myFile.write(f"extern float pca_components[PCA_N_FEATURE][PCA_N_ORIG_FEATURE];\n")
     myFile.write(f"\n")
 
     if scaler!=None:
         if isinstance(scaler, preprocessing.StandardScaler):
             myFile.write(f"#define STANDARD_SCALING\n\n")
-            myFile.write(f"extern float s_x[N_ORIG_FEATURE];\n")
-            myFile.write(f"extern float u_x[N_ORIG_FEATURE];\n")
+            myFile.write(f"extern float s_x[PCA_N_ORIG_FEATURE];\n")
+            myFile.write(f"extern float u_x[PCA_N_ORIG_FEATURE];\n")
         elif isinstance(scaler, preprocessing.MinMaxScaler):
             myFile.write(f"#define MINMAX_SCALING\n\n")
-            myFile.write(f"extern float s_x[N_ORIG_FEATURE];\n")
+            myFile.write(f"extern float s_x[PCA_N_ORIG_FEATURE];\n")
 
     '''
     if cfg.normalization!=None and cfg.regr and cfg.algo.lower() != 'dt':
@@ -60,19 +60,19 @@ def savePPParams(scaler, reduce_dims, estimator):
     import sys
     sys.path.insert(1, 'utils')
     import create_matrices
-    stri = create_matrices.createMatrix('float', 'pca_components', pca_components, 'N_FEATURE', 'N_ORIG_FEATURE')
+    stri = create_matrices.createMatrix('float', 'pca_components', pca_components, 'PCA_N_FEATURE', 'PCA_N_ORIG_FEATURE')
     myFile.write(stri)
     myFile.write(f"\n")
 
     if scaler!=None:
         if isinstance(scaler, preprocessing.StandardScaler):
             myFile.write(f"#define STANDARD_SCALING\n\n")
-            stri = create_matrices.createArray('float', "s_x", np.reshape(sx, (sx.shape[0], )), 'N_ORIG_FEATURE')
+            stri = create_matrices.createArray('float', "s_x", np.reshape(sx, (sx.shape[0], )), 'PCA_N_ORIG_FEATURE')
             myFile.write(stri)
-            stri = create_matrices.createArray('float', "u_x", np.reshape(ux, (ux.shape[0], )), 'N_ORIG_FEATURE')
+            stri = create_matrices.createArray('float', "u_x", np.reshape(ux, (ux.shape[0], )), 'PCA_N_ORIG_FEATURE')
             myFile.write(stri)
         elif isinstance(scaler, preprocessing.MinMaxScaler):
             myFile.write(f"#define MINMAX_SCALING\n\n")
-            stri = create_matrices.createArray('float', "s_x", np.reshape(sx, (sx.shape[0], )), 'N_ORIG_FEATURE')
+            stri = create_matrices.createArray('float', "s_x", np.reshape(sx, (sx.shape[0], )), 'PCA_N_ORIG_FEATURE')
             myFile.write(stri)
     myFile.close()
