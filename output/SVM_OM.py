4ff980dc52211a6b28dc73da70a6991f9b28a6f1
Fouad Sakr <57620529+FouadSakr@users.noreply.github.com>
2022-09-15 15:35:51 +0100

    Update SVM_OM.py for multi-class classification

--- output/SVM_OM.py
+++ output/SVM_OM.py
@@ -1,4 +1,5 @@
 import abc
+import numpy as np
 from OutputMgr import OutputMgr
 
 import numpy as np
@@ -7,7 +8,17 @@ class SVM_OM(OutputMgr):
     def __init__(self, estimator):
         super().__init__()
         self.estimator = estimator
-
+        
+    def get_combinations(self, best_estimator):
+        classes = best_estimator.classes_
+        compinations = []
+        for i in range(len(classes):
+            for j in range(i+1, len(classes)):
+                compinations.append([classes[i], classes[j]])
+        combinations = np.array(combinations)
+        combinations = combinations.T
+        return combinations
+                       
     def saveParams(self, best_estimator):
         #outdir = OutputMgr.checkCreateDSDir(self.estimator.dataset.name, self.estimator.nick)
         outdirI = OutputMgr.getOutIncludeDir()
@@ -30,6 +41,7 @@ class SVM_OM(OutputMgr):
 
         if self.estimator.is_regr == False:
             myFile.write(f"extern float support_vectors[WEIGTH_DIM][N_FEATURE];\n")
+            myFile.write(f"extern int Truth_Table[2][WEIGTH_DIM];\n")
         else: #Same as above as a declaration
             myFile.write(f"extern float support_vectors[WEIGTH_DIM][N_FEATURE];\n")
         
@@ -46,9 +58,12 @@ class SVM_OM(OutputMgr):
         import create_matrices
         if self.estimator.is_regr == False:
             stri = create_matrices.createMatrix('float', 'support_vectors', best_estimator.coef_, 'WEIGTH_DIM', 'N_FEATURE')
+            truth_table = get_combinations(best_estimator)
+            stri1 = create_matrices.createMatrix('int', 'Truth_Table', truth_table, '2', 'WEIGTH_DIM')
         else:
             stri = create_matrices.createMatrix("float", "support_vectors", np.reshape(best_estimator.coef_, (1, best_estimator.coef_.shape[1])), 'WEIGTH_DIM', 'N_FEATURE')   
         myFile.write(stri)
+        myFile.write(stri1)
         stri = create_matrices.createArray("float", "bias", best_estimator.intercept_, 'WEIGTH_DIM')
         myFile.write(stri)
         myFile.close()
